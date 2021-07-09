47d7ac5083fe0ec5c973f8ec692efef3576cbd6f
ict283 <glennlum@icloud.com>
2021-07-09 18:34:37 +0800

    Link dependencies

--- workingWithBSTs/Dependencies/Dependencies.h
+++ workingWithBSTs/Dependencies/Dependencies.h
@@ -5,6 +5,14 @@
 #ifndef Dependencies_h
 #define Dependencies_h
 
-
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include "../Classes/Date.h"
+#include "../Classes/Person.h"
+#include "../Classes/BST.h"
+#include "../Classes/Wrapper.h"
+#include "../Processors/Processors.h"
+#include "../Processors/KeyGen.h"
 
 #endif /* Dependencies_h */
